d807583cf21c334fe26ecc37ee58e53ad89a2a7d
MayurSabade <79620475+mayur0319@users.noreply.github.com>
2025-10-15 13:44:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,15 +41,3 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
   policy = data.aws_iam_policy_document.bucket_policy_document.json
 }
-#upload website files to s3:
-resource "aws_s3_object" "object" {
-  bucket       = aws_s3_bucket.bucket.id
-  for_each     = fileset("uploads/", "*")
-  key          = "website/${each.value}"
-  source       = "uploads/${each.value}"
-  etag         = filemd5("uploads/${each.value}")
-  content_type = "text/html"
-  depends_on = [
-    aws_s3_bucket.bucket
-  ]
-}
